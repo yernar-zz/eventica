56e4149a408486b02994474b1bc11ebde111a2d6
Askar Absemetov <askar.bsm@gmail.com>
2013-02-03 21:27:13 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventica::Application.config.secret_token = 'af4d8beaa480fdc043c8ead56d044641e06201d65d21ef089b275869161811c398d4f1b524c08143da6461d362c1f25e6229bd5811baa412129e8bf4d657405f'
