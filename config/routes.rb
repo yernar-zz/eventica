5bba3953cfa70db83b1bc1e653c01a5d6e0cdfa3
Askar <ask.register@gmail.com>
2013-02-22 18:39:16 +0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Eventica::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/steps', to: 'static_pages#steps'
   match '/about', to: 'static_pages#about'
