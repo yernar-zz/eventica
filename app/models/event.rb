c076afd52d7e3091de31c9b936fb60c227fee5f6
Askar <ask.register@gmail.com>
2013-03-04 19:20:19 +0600

    minor changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  user_id     :integer
+#  description :string(255)
+#  venue       :string(255)
+#  start_date  :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  end_date    :datetime
+#
+
 class Event < ActiveRecord::Base
   attr_accessible :start_date, :end_date, :description, :title, :venue
   belongs_to :user
@@ -10,5 +25,5 @@ class Event < ActiveRecord::Base
   validates :end_date, presence: true
 
 
-  default_scope order: 'events.created_at DESC'
+  default_scope order: 'events.start_date DESC'
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  user_id     :integer
+#  description :string(255)
+#  venue       :string(255)
+#  start_date  :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  end_date    :datetime
+#
+
 require 'spec_helper'
 
 describe Event do
